b46a2734c27ee4ea102f403e4fac68aa3ff78179
guorui.xgr <guorui.xgr@alibaba-inc.com>
2018-12-11 13:34:47 +0800

    fix parameters of dice and prelu

--- din/Dice.py
+++ din/Dice.py
@@ -1,9 +1,13 @@
 import tensorflow as tf
 
 def dice(_x, axis=-1, epsilon=0.000000001, name=''):
-  alphas = tf.get_variable('alpha'+name, _x.get_shape()[-1],                                  
-                       initializer=tf.constant_initializer(0.0),                         
-                       dtype=tf.float32)
+  with tf.variable_scope(name_or_scope='', reuse=tf.AUTO_REUSE):
+    alphas = tf.get_variable('alpha'+name, _x.get_shape()[-1],                                  
+                         initializer=tf.constant_initializer(0.0),                         
+                         dtype=tf.float32)
+    beta = tf.get_variable('beta'+name, _x.get_shape()[-1],                                  
+                         initializer=tf.constant_initializer(0.0),                         
+                         dtype=tf.float32)
   input_shape = list(_x.get_shape())
 
   reduction_axes = list(range(len(input_shape)))
@@ -17,17 +21,18 @@ def dice(_x, axis=-1, epsilon=0.000000001, name=''):
   std = tf.reduce_mean(tf.square(_x - brodcast_mean) + epsilon, axis=reduction_axes)
   std = tf.sqrt(std)
   brodcast_std = tf.reshape(std, broadcast_shape)
-  #x_normed = (_x - brodcast_mean) / (brodcast_std + epsilon)
-  x_normed = tf.layers.batch_normalization(_x, center=False, scale=False) #a simple way to use BN to calculate x_p
-  x_p = tf.sigmoid(x_normed)
+  x_normed = tf.layers.batch_normalization(_x, center=False, scale=False, name=name, reuse=tf.AUTO_REUSE)
+  # x_normed = (_x - brodcast_mean) / (brodcast_std + epsilon)
+  x_p = tf.sigmoid(beta * x_normed)
  
   
   return alphas * (1.0 - x_p) * _x + x_p * _x
 
-def parametric_relu(_x, name=''):
-  alphas = tf.get_variable('alpha' + name, _x.get_shape()[-1],
-                       initializer=tf.constant_initializer(0.0),
-                       dtype=tf.float32)
+def parametric_relu(_x):
+  with tf.variable_scope(name_or_scope='', reuse=tf.AUTO_REUSE):
+    alphas = tf.get_variable('alpha', _x.get_shape()[-1],
+                         initializer=tf.constant_initializer(0.0),
+                         dtype=tf.float32)
   pos = tf.nn.relu(_x)
   neg = alphas * (_x - abs(_x)) * 0.5
 
--- din/model_dice.py
+++ din/model_dice.py
@@ -4,7 +4,7 @@ from Dice import dice
 
 class Model(object):
 
-  def __init__(self, user_count, item_count, cate_count, cate_list):
+  def __init__(self, user_count, item_count, cate_count, cate_list, predict_batch_size, predict_ads_num):
 
     self.u = tf.placeholder(tf.int32, [None,]) # [B]
     self.i = tf.placeholder(tf.int32, [None,]) # [B]
@@ -22,8 +22,6 @@ class Model(object):
                              initializer=tf.constant_initializer(0.0))
     cate_emb_w = tf.get_variable("cate_emb_w", [cate_count, hidden_units // 2])
     cate_list = tf.convert_to_tensor(cate_list, dtype=tf.int64)
-
-    u_emb = tf.nn.embedding_lookup(user_emb_w, self.u)
 
     ic = tf.gather(cate_list, self.i)
     i_emb = tf.concat(values = [
@@ -45,58 +43,87 @@ class Model(object):
         tf.nn.embedding_lookup(cate_emb_w, hc),
         ], axis=2)
 
-    hist =attention(i_emb, h_emb, self.sl)
+    hist_i =attention(i_emb, h_emb, self.sl)
     #-- attention end ---
     
-    hist = tf.layers.batch_normalization(inputs = hist)
-    hist = tf.reshape(hist, [-1, hidden_units])
-    hist = tf.layers.dense(hist, hidden_units)
+    hist_i = tf.layers.batch_normalization(inputs = hist_i)
+    hist_i = tf.reshape(hist_i, [-1, hidden_units], name='hist_bn')
+    hist_i = tf.layers.dense(hist_i, hidden_units, name='hist_fcn')
 
-    u_emb = hist
-    print u_emb.get_shape().as_list()
+    u_emb_i = hist_i
+    
+    hist_j =attention(j_emb, h_emb, self.sl)
+    #-- attention end ---
+    
+    hist_j = tf.layers.batch_normalization(inputs = hist_j)
+    hist_j = tf.reshape(hist_j, [-1, hidden_units], name='hist_bn')
+    hist_j = tf.layers.dense(hist_j, hidden_units, name='hist_fcn', reuse=True)
+
+    u_emb_j = hist_j
+    print u_emb_i.get_shape().as_list()
+    print u_emb_j.get_shape().as_list()
     print i_emb.get_shape().as_list()
     print j_emb.get_shape().as_list()
     #-- fcn begin -------
-    din_i = tf.concat([u_emb, i_emb], axis=-1)
+    din_i = tf.concat([u_emb_i, i_emb], axis=-1)
     din_i = tf.layers.batch_normalization(inputs=din_i, name='b1')
+    #if u want try dice change sigmoid to None and add dice layer like following two lines. You can also find model_dice.py in this folder.
     d_layer_1_i = tf.layers.dense(din_i, 80, activation=None, name='f1')
-    d_layer_1_i = dice(d_layer_1_i, name='dice_1_i')
+    d_layer_1_i = dice(d_layer_1_i, name='dice_1')
     d_layer_2_i = tf.layers.dense(d_layer_1_i, 40, activation=None, name='f2')
-    d_layer_2_i = dice(d_layer_2_i, name='dice_2_i')
+    d_layer_2_i = dice(d_layer_2_i, name='dice_2')
     d_layer_3_i = tf.layers.dense(d_layer_2_i, 1, activation=None, name='f3')
-    din_j = tf.concat([u_emb, j_emb], axis=-1)
+    din_j = tf.concat([u_emb_j, j_emb], axis=-1)
     din_j = tf.layers.batch_normalization(inputs=din_j, name='b1', reuse=True)
     d_layer_1_j = tf.layers.dense(din_j, 80, activation=None, name='f1', reuse=True)
-    d_layer_1_j = dice(d_layer_1_j, name='dice_1_j')
+    d_layer_1_j = dice(d_layer_1_j, name='dice_1')
     d_layer_2_j = tf.layers.dense(d_layer_1_j, 40, activation=None, name='f2', reuse=True)
-    d_layer_2_j = dice(d_layer_2_j, name='dice_2_j')
+    d_layer_2_j = dice(d_layer_2_j, name='dice_2')
     d_layer_3_j = tf.layers.dense(d_layer_2_j, 1, activation=None, name='f3', reuse=True)
     d_layer_3_i = tf.reshape(d_layer_3_i, [-1])
     d_layer_3_j = tf.reshape(d_layer_3_j, [-1])
     x = i_b - j_b + d_layer_3_i - d_layer_3_j # [B]
     self.logits = i_b + d_layer_3_i
-    u_emb_all = tf.expand_dims(u_emb, 1)
-    u_emb_all = tf.tile(u_emb_all, [1, item_count, 1])
-    # logits for all item:
-    all_emb = tf.concat([
+    
+    # prediciton for selected items
+    # logits for selected item:
+    item_emb_all = tf.concat([
         item_emb_w,
         tf.nn.embedding_lookup(cate_emb_w, cate_list)
         ], axis=1)
-    all_emb = tf.expand_dims(all_emb, 0)
-    all_emb = tf.tile(all_emb, [512, 1, 1])
-    din_all = tf.concat([u_emb_all, all_emb], axis=-1)
-    din_all = tf.layers.batch_normalization(inputs=din_all, name='b1', reuse=True)
-    d_layer_1_all = tf.layers.dense(din_all, 80, activation=None, name='f1', reuse=True)
-    d_layer_1_all = dice(d_layer_1_all, name='dice_1_all')
-    d_layer_2_all = tf.layers.dense(d_layer_1_all, 40, activation=None, name='f2', reuse=True)
-    d_layer_2_all = dice(d_layer_2_all, name='dice_2_all')
-    d_layer_3_all = tf.layers.dense(d_layer_2_all, 1, activation=None, name='f3', reuse=True)
-    d_layer_3_all = tf.reshape(d_layer_3_all, [-1, item_count])
-    self.logits_all = tf.sigmoid(item_b + d_layer_3_all)
+    item_emb_sub = item_emb_all[:predict_ads_num,:]
+    item_emb_sub = tf.expand_dims(item_emb_sub, 0)
+    item_emb_sub = tf.tile(item_emb_sub, [predict_batch_size, 1, 1])
+    hist_sub =attention_multi_items(item_emb_sub, h_emb, self.sl)
+    #-- attention end ---
+    
+    hist_sub = tf.layers.batch_normalization(inputs = hist_sub, name='hist_bn', reuse=tf.AUTO_REUSE)
+    # print hist_sub.get_shape().as_list() 
+    hist_sub = tf.reshape(hist_sub, [-1, hidden_units])
+    hist_sub = tf.layers.dense(hist_sub, hidden_units, name='hist_fcn', reuse=tf.AUTO_REUSE)
+
+    u_emb_sub = hist_sub
+    item_emb_sub = tf.reshape(item_emb_sub, [-1, hidden_units])
+    din_sub = tf.concat([u_emb_sub, item_emb_sub], axis=-1)
+    din_sub = tf.layers.batch_normalization(inputs=din_sub, name='b1', reuse=True)
+    d_layer_1_sub = tf.layers.dense(din_sub, 80, activation=tf.nn.sigmoid, name='f1', reuse=True)
+    #d_layer_1_sub = dice(d_layer_1_sub, name='dice_1_sub')
+    d_layer_2_sub = tf.layers.dense(d_layer_1_sub, 40, activation=tf.nn.sigmoid, name='f2', reuse=True)
+    #d_layer_2_sub = dice(d_layer_2_sub, name='dice_2_sub')
+    d_layer_3_sub = tf.layers.dense(d_layer_2_sub, 1, activation=None, name='f3', reuse=True)
+    d_layer_3_sub = tf.reshape(d_layer_3_sub, [-1, predict_ads_num])
+    self.logits_sub = tf.sigmoid(item_b[:predict_ads_num] + d_layer_3_sub)
+    self.logits_sub = tf.reshape(self.logits_sub, [-1, predict_ads_num, 1])
     #-- fcn end -------
 
     
     self.mf_auc = tf.reduce_mean(tf.to_float(x > 0))
+    self.score_i = tf.sigmoid(i_b + d_layer_3_i)
+    self.score_j = tf.sigmoid(j_b + d_layer_3_j)
+    self.score_i = tf.reshape(self.score_i, [-1, 1])
+    self.score_j = tf.reshape(self.score_j, [-1, 1])
+    self.p_and_n = tf.concat([self.score_i, self.score_j], axis=-1)
+    print self.p_and_n.get_shape().as_list()
 
 
     # Step variable
@@ -132,21 +159,24 @@ class Model(object):
     return loss
 
   def eval(self, sess, uij):
-    u_auc = sess.run(self.mf_auc, feed_dict={
+    u_auc, socre_p_and_n = sess.run([self.mf_auc, self.p_and_n], feed_dict={
         self.u: uij[0],
         self.i: uij[1],
         self.j: uij[2],
         self.hist_i: uij[3],
         self.sl: uij[4],
         })
-    return u_auc
-
-  def test(self, sess, uid, hist_i, sl):
-    return sess.run(self.logits_all, feed_dict={
-        self.u: uid,
-        self.hist_i: hist_i,
-        self.sl: sl,
+    return u_auc, socre_p_and_n
+  
+  def test(self, sess, uij):
+    return sess.run(self.logits_sub, feed_dict={
+        self.u: uij[0],
+        self.i: uij[1],
+        self.j: uij[2],
+        self.hist_i: uij[3],
+        self.sl: uij[4],
         })
+  
 
   def save(self, sess, path):
     saver = tf.train.Saver()
@@ -172,9 +202,9 @@ def attention(queries, keys, keys_length):
   queries = tf.tile(queries, [1, tf.shape(keys)[1]])
   queries = tf.reshape(queries, [-1, tf.shape(keys)[1], queries_hidden_units])
   din_all = tf.concat([queries, keys, queries-keys, queries*keys], axis=-1)
-  d_layer_1_all = tf.layers.dense(din_all, 80, activation=tf.nn.sigmoid, name='f1_att')
-  d_layer_2_all = tf.layers.dense(d_layer_1_all, 40, activation=tf.nn.sigmoid, name='f2_att')
-  d_layer_3_all = tf.layers.dense(d_layer_2_all, 1, activation=None, name='f3_att')
+  d_layer_1_all = tf.layers.dense(din_all, 80, activation=tf.nn.sigmoid, name='f1_att', reuse=tf.AUTO_REUSE)
+  d_layer_2_all = tf.layers.dense(d_layer_1_all, 40, activation=tf.nn.sigmoid, name='f2_att', reuse=tf.AUTO_REUSE)
+  d_layer_3_all = tf.layers.dense(d_layer_2_all, 1, activation=None, name='f3_att', reuse=tf.AUTO_REUSE)
   d_layer_3_all = tf.reshape(d_layer_3_all, [-1, 1, tf.shape(keys)[1]])
   outputs = d_layer_3_all 
   # Mask
@@ -194,3 +224,43 @@ def attention(queries, keys, keys_length):
 
   return outputs
 
+def attention_multi_items(queries, keys, keys_length):
+  '''
+    queries:     [B, N, H] N is the number of ads
+    keys:        [B, T, H] 
+    keys_length: [B]
+  '''
+  queries_hidden_units = queries.get_shape().as_list()[-1]
+  queries_nums = queries.get_shape().as_list()[1]
+  queries = tf.tile(queries, [1, 1, tf.shape(keys)[1]])
+  queries = tf.reshape(queries, [-1, queries_nums, tf.shape(keys)[1], queries_hidden_units]) # shape : [B, N, T, H]
+  max_len = tf.shape(keys)[1]
+  keys = tf.tile(keys, [1, queries_nums, 1])
+  keys = tf.reshape(keys, [-1, queries_nums, max_len, queries_hidden_units]) # shape : [B, N, T, H]
+  din_all = tf.concat([queries, keys, queries-keys, queries*keys], axis=-1)
+  d_layer_1_all = tf.layers.dense(din_all, 80, activation=tf.nn.sigmoid, name='f1_att', reuse=tf.AUTO_REUSE)
+  d_layer_2_all = tf.layers.dense(d_layer_1_all, 40, activation=tf.nn.sigmoid, name='f2_att', reuse=tf.AUTO_REUSE)
+  d_layer_3_all = tf.layers.dense(d_layer_2_all, 1, activation=None, name='f3_att', reuse=tf.AUTO_REUSE)
+  d_layer_3_all = tf.reshape(d_layer_3_all, [-1, queries_nums, 1, max_len])
+  outputs = d_layer_3_all 
+  # Mask
+  key_masks = tf.sequence_mask(keys_length, max_len)   # [B, T]
+  key_masks = tf.tile(key_masks, [1, queries_nums])
+  key_masks = tf.reshape(key_masks, [-1, queries_nums, 1, max_len]) # shape : [B, N, 1, T]
+  paddings = tf.ones_like(outputs) * (-2 ** 32 + 1)
+  outputs = tf.where(key_masks, outputs, paddings)  # [B, N, 1, T]
+
+  # Scale
+  outputs = outputs / (keys.get_shape().as_list()[-1] ** 0.5)
+
+  # Activation
+  outputs = tf.nn.softmax(outputs)  # [B, N, 1, T]
+  outputs = tf.reshape(outputs, [-1, 1, max_len])
+  keys = tf.reshape(keys, [-1, max_len, queries_hidden_units])
+  #print outputs.get_shape().as_list()
+  #print keys.get_sahpe().as_list()
+  # Weighted sum
+  outputs = tf.matmul(outputs, keys)
+  outputs = tf.reshape(outputs, [-1, queries_nums, queries_hidden_units])  # [B, N, 1, H]
+  print outputs.get_shape().as_list()
+  return outputs
